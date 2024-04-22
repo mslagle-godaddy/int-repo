46b69e74d2ff96f8e9663c33a05bd48716a67178
Mike Slagle <=>
2024-04-22 12:50:42 -0700

    adding comment for where to put view

--- app/src/main/java/com/example/intapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/intapp/ui/MainActivity.kt
@@ -17,13 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             IntAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    /* TODO */
-                }
+                // TODO: Add your app view here
             }
         }
     }
