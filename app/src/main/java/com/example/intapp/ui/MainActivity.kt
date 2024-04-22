0fcc8ddc251f9c02e960f92b5fa726a285b698cb
Mike Slagle <=>
2024-04-22 12:59:31 -0700

    adding coroutines test version

--- app/src/main/java/com/example/intapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/intapp/ui/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             IntAppTheme {
                 // TODO: Add your app view here
+                Text(text = "Hello!")
             }
         }
     }
