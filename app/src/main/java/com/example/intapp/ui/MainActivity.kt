3eabe471e35ef54444db22c153179c7e625fe397
Mike Slagle <=>
2024-04-22 16:12:32 -0700

    changing theme'

--- app/src/main/java/com/example/intapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/intapp/ui/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.intapp.ui
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.Text
 import com.example.intapp.ui.theme.IntAppTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
